606ec22d5901d384b1f9d063c3c44e11659a1136
emge1 <emygeq@gmail.com>
2024-12-06 13:50:29 +0100

    #39 improve tests

--- v1/categories/tests/test_integration_category.py
+++ v1/categories/tests/test_integration_category.py
@@ -3,12 +3,17 @@ from v1.categories.models.category import Category
 from v1.subcategories.models.subcategory import Subcategory
 from v1.products.models.product import Product
 from rest_framework.test import APITestCase, APIClient
+from django.contrib.auth import get_user_model
+from v1.user_roles.models.merchant import Merchant
 
 
 @pytest.mark.django_db
 @pytest.mark.integration
-class TestCotegoryViews(APITestCase):
+class TestCategoryViews(APITestCase):
     def setUp(self) -> None:
+        User = get_user_model()
+        self.user = User.objects.create_user(email="example111@example.com", password="password", type_choice=3)
+        self.merchant = Merchant.objects.create(user=self.user)
         self.client = APIClient()
         self.category = Category.objects.create(title="Test category")
 
@@ -38,7 +43,8 @@ class TestCotegoryViews(APITestCase):
             max_price=99.99,
             thumbnail="thumbnail44.jpg",
             is_active=True,
-            subcategories_id=self.subcategory1
+            subcategories_id=self.subcategory1,
+            added_by=self.merchant
         )
 
         self.product2 = Product.objects.create(
@@ -46,29 +52,30 @@ class TestCotegoryViews(APITestCase):
             max_price=99.99,
             thumbnail="thumbnail5.jpg",
             is_active=True,
-            subcategories_id=self.subcategory1
+            subcategories_id=self.subcategory1,
+            added_by=self.merchant
         )
 
     def test_get_subcategories(self):
-        response = self.client.get(f"/categories/{self.category.id}/subcategories")
+        response = self.client.get(f"/categories/{self.category.id}/subcategories/")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.json), 3)
+        self.assertEqual(len(response.json()), 3)
         self.assertEqual(response.json()[0]["title"], "Subcategory 1")
-        self.assertEqual(response.json()[0]["thumbnail"], "thumbnail1.jpg")
+        self.assertEqual(response.json()[0]["thumbnail"], "/media/thumbnail1.jpg")
         self.assertEqual(response.json()[1]["title"], "Subcategory 2")
-        self.assertEqual(response.json()[1]["thumbnail"], "thumbnail2.jpg")
+        self.assertEqual(response.json()[1]["thumbnail"], "/media/thumbnail2.jpg")
         self.assertEqual(response.json()[2]["title"], "Subcategory 3")
-        self.assertEqual(response.json()[2]["thumbnail"], "thumbnail3.jpg")
+        self.assertEqual(response.json()[2]["thumbnail"], "/media/thumbnail3.jpg")
 
     def test_get_products(self):
         response = self.client.get(f"/categories/{self.category.id}/products")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.json), 2)
+        self.assertEqual(len(response.json()), 2)
         self.assertEqual(response.json()[0]["title"], "Product 1")
         self.assertEqual(response.json()[0]["price"], 99.99)
-        self.assertEqual(response.json()[0]["thumbnail"], "thumbnail4.jpg")
+        self.assertEqual(response.json()[0]["thumbnail"], "/media/thumbnail4.jpg")
         self.assertEqual(response.json()[1]["title"], "Product 2")
         self.assertEqual(response.json()[1]["price"], 99.99)
-        self.assertEqual(response.json()[1]["thumbnail"], "thumbnail5.jpg")
+        self.assertEqual(response.json()[1]["thumbnail"], "/media/thumbnail5.jpg")
 
 
