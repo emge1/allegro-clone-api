875090f21b3e378dc2ec671edccd5a983d84cd7c
emge1 <emygeq@gmail.com>
2024-12-17 18:19:58 +0100

    #95 - Feature: load_sample_data.sh for SQLite (dev)

--- load_sample_data.sh
+++ load_sample_data.sh
@@ -1,4 +1,42 @@
 #!/usr/bin/env bash
 
-# accounts
-python manage.py loaddata v1/accounts/fixtures/users.json
+DB_FILE="config/db.sqlite3"
+SQL_SAMPLE_DATA_DIR="./sample_data"
+
+# Sprawdzenie katalogu i pliku bazy danych
+if [ ! -d "$SQL_SAMPLE_DATA_DIR" ]; then
+  echo "Error: $SQL_SAMPLE_DATA_DIR does not exist."
+  exit 1
+fi
+
+if [ ! -f "$DB_FILE" ]; then
+  echo "Error: $DB_FILE does not exist."
+  exit 1
+fi
+
+for file in "$SQL_SAMPLE_DATA_DIR"/*.sql; do
+  [ -e "$file" ] || { echo "No .sql files in '$SQL_SAMPLE_DATA_DIR'"; exit 1; }
+
+  table_name=$(basename "$file" .sql)
+  echo "Checking table: '$table_name'"
+
+  row_count=$(sqlite3 "$DB_FILE" "SELECT COUNT(*) FROM $table_name;" 2>/dev/null)
+  if [ $? -ne 0 ]; then
+    echo "Table '$table_name' does not exist. Skipping import for '$file'."
+    continue
+  fi
+
+  if [ "$row_count" -eq 0 ]; then
+    echo "The table '$table_name' is empty. Importing data from '$file'..."
+    sqlite3 "$DB_FILE" < "$file"
+    if [ $? -eq 0 ]; then
+      echo "Data successfully imported to '$table_name'."
+    else
+      echo "Error while importing data from '$file' to '$table_name'."
+    fi
+  else
+    echo "The table '$table_name' is not empty. Skipping import."
+  fi
+done
+
+echo "Sample data import finished."
